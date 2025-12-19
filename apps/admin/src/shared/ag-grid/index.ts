673c975369456c8cebd6e2f5e8996de5a0f0d7b6
Bruce <recall4056@gmail.com>
2025-12-19 08:08:31 +0800

    chore(admin): :hammer: add aggrid key

--- apps/admin/src/shared/ag-grid/index.ts
+++ apps/admin/src/shared/ag-grid/index.ts
@@ -2,7 +2,9 @@ import { AG_GRID_LOCALE_CN } from '@ag-grid-community/locale'
 import { ModuleRegistry, provideGlobalGridOptions } from 'ag-grid-community'
 import { AllEnterpriseModule, LicenseManager } from 'ag-grid-enterprise'
 
-LicenseManager.setLicenseKey('')
+LicenseManager.setLicenseKey(
+  '[v3][RELEASE][0102]_NDg2Njc4MzY3MDgzNw==16d78ca762fb5d2ff740aed081e2af7b'
+)
 
 ModuleRegistry.registerModules([AllEnterpriseModule])
 
